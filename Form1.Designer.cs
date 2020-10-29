3f793493cd428f307d700414ecd1b9db6338fcff
Brendan Caldwell <40771777+Brednan@users.noreply.github.com>
2020-10-28 18:24:04 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace PotentialEnergyCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.massInput = new System.Windows.Forms.TextBox();
+            this.MassLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gravityInput = new System.Windows.Forms.TextBox();
+            this.height = new System.Windows.Forms.Label();
+            this.HeightInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(141, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(818, 73);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Potenial Energy Calculator";
+            // 
+            // massInput
+            // 
+            this.massInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.massInput.Location = new System.Drawing.Point(242, 184);
+            this.massInput.Name = "massInput";
+            this.massInput.Size = new System.Drawing.Size(275, 62);
+            this.massInput.TabIndex = 2;
+            // 
+            // MassLabel
+            // 
+            this.MassLabel.AutoSize = true;
+            this.MassLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MassLabel.Location = new System.Drawing.Point(29, 191);
+            this.MassLabel.Name = "MassLabel";
+            this.MassLabel.Size = new System.Drawing.Size(152, 55);
+            this.MassLabel.TabIndex = 3;
+            this.MassLabel.Text = "Mass:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 308);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 55);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Gravity:";
+            // 
+            // gravityInput
+            // 
+            this.gravityInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gravityInput.Location = new System.Drawing.Point(242, 308);
+            this.gravityInput.Name = "gravityInput";
+            this.gravityInput.Size = new System.Drawing.Size(275, 62);
+            this.gravityInput.TabIndex = 5;
+            // 
+            // height
+            // 
+            this.height.AutoSize = true;
+            this.height.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.height.Location = new System.Drawing.Point(29, 418);
+            this.height.Name = "height";
+            this.height.Size = new System.Drawing.Size(176, 55);
+            this.height.TabIndex = 6;
+            this.height.Text = "Height:";
+            // 
+            // HeightInput
+            // 
+            this.HeightInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HeightInput.Location = new System.Drawing.Point(242, 415);
+            this.HeightInput.Name = "HeightInput";
+            this.HeightInput.Size = new System.Drawing.Size(275, 62);
+            this.HeightInput.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(675, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 59);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // output
+            // 
+            this.output.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.Location = new System.Drawing.Point(675, 291);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(193, 62);
+            this.output.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 633);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.HeightInput);
+            this.Controls.Add(this.height);
+            this.Controls.Add(this.gravityInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.MassLabel);
+            this.Controls.Add(this.massInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox massInput;
+        private System.Windows.Forms.Label MassLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox gravityInput;
+        private System.Windows.Forms.Label height;
+        private System.Windows.Forms.TextBox HeightInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox output;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PotentialEnergyCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static float PotentialEnergyCalculator(float m, float g, float h)
+        {
+            float result = m * g * h;
+            return result;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float gravity = float.Parse(gravityInput.Text);
+            float mass = float.Parse(massInput.Text);
+            float height = float.Parse(HeightInput.Text);
+
+            output.Text = PotentialEnergyCalculator(mass, gravity, height).ToString();
+        }
+    }
+}
